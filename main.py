333e7fdfc1454bba546a0f4ff5860dc165c8fafe
havison <dieu2010@mail.ru>
2024-12-16 13:08:06 +1000

    Добавлен объем

--- main.py
+++ main.py
@@ -110,8 +110,6 @@ async def main():
                                 quantity.setdefault(symbol, []).append(datetime.now())
                                 q = len(quantity[symbol])
                                 write_file(oi_file, quantity)
-                                if q <= 3:
-                                    await message_my(symbol, a, oi, q, volume, volume24)
                             if symbol in bybit_symbol and symbol in binance_symbol:
                                 await message_bybit_binance(symbol, a, oi, q, volume, volume24)
                                 await asyncio.sleep(1)
